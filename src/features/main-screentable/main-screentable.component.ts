9b83d6df03030f502d612cf35cf905ebf085f325
Nikita Cherevko <nikita.cherevko@vk.team>
2023-12-21 18:33:33 +0300

    update grid

--- src/features/main-screentable/main-screentable.component.ts
+++ src/features/main-screentable/main-screentable.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatGridListModule} from "@angular/material/grid-list";
 
 @Component({
   selector: 'app-main-screentable',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatGridListModule],
   templateUrl: './main-screentable.component.html',
   styleUrl: './main-screentable.component.scss',
 })
