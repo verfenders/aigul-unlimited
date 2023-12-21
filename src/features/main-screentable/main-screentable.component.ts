e167cd7c7817c0ab993110d965911d162e1988b5
Nikita Cherevko <nikita.cherevko@vk.team>
2023-12-21 19:45:01 +0300

    create inner grid section

--- src/features/main-screentable/main-screentable.component.ts
+++ src/features/main-screentable/main-screentable.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatGridListModule} from "@angular/material/grid-list";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-main-screentable',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatGridListModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatGridListModule, MatDividerModule],
   templateUrl: './main-screentable.component.html',
   styleUrl: './main-screentable.component.scss',
 })
