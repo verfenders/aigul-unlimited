422671905bf7a43a4187187fc6483bf12be1b940
Nikita Cherevko <nikita.cherevko@vk.team>
2024-01-10 23:38:45 +0300

    update progress design

--- src/features/main-screentable/main-screentable.component.ts
+++ src/features/main-screentable/main-screentable.component.ts
@@ -4,20 +4,27 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatDialog} from "@angular/material/dialog";
-import {TaskFaqComponent} from "../task-faq/task-faq.component";
+import { MatDialog } from '@angular/material/dialog';
+import { TaskFaqComponent } from '../task-faq/task-faq.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-main-screentable',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTooltipModule, MatGridListModule, MatDividerModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './main-screentable.component.html',
   styleUrl: './main-screentable.component.scss',
 })
 export class MainScreentableComponent {
+  constructor(private dialog: MatDialog) {}
 
-  constructor(private dialog: MatDialog) {
-  }
   openFaq() {
     this.dialog.open(TaskFaqComponent);
   }
