a275ee047abd324294fe4b90996bfe914387cdab
Nikita Cherevko <nikita.cherevko@vk.team>
2024-01-11 23:36:49 +0300

    add auth form

--- src/features/auth/auth.component.ts
+++ src/features/auth/auth.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
+  personId = new FormControl('', [Validators.required]);
+  loading = false;
 
+  login() {
+    this.loading = true;
+    console.log('event - ', this.personId.value);
+  }
 }
