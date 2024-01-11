8509afde2b82c296ec7b3d931a9d3d53d32fee0c
Nikita Cherevko <nikita.cherevko@vk.team>
2024-01-11 20:50:44 +0300

    create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
 import { MainScreentableComponent } from '../features/main-screentable/main-screentable.component';
+import { AuthComponent } from '../features/auth/auth.component';
+import { EmptyRouteComponent } from '../features/empty-route/empty-route.component';
 
 export const routes: Routes = [
   {
+    path: 'work-space',
+    component: MainScreentableComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+  },
+  {
     path: '',
-    component: MainScreentableComponent,
+    pathMatch: 'full',
+    redirectTo: '/auth'
+  },
+  {
+    path: '**',
+    component: EmptyRouteComponent
   },
 ];
