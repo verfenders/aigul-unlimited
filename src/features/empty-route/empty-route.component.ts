475e4e364b1dace868a0b49fc6765884b15ae57b
Nikita Cherevko <nikita.cherevko@vk.team>
2024-01-31 23:34:29 +0300

    add empty page

--- src/features/empty-route/empty-route.component.ts
+++ src/features/empty-route/empty-route.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-empty-route',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './empty-route.component.html',
-  styleUrl: './empty-route.component.scss'
+  styleUrl: './empty-route.component.scss',
 })
-export class EmptyRouteComponent {
-
-}
+export class EmptyRouteComponent {}
